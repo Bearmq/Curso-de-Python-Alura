0077ad9b0bb729c9231fdf9239d2260a0b8067e0
Beatriz <beatrizriosmarques@gmail.com>
2024-03-21 18:31:11 -0300

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Xan', 11)
-restaurante_praca.receber_avaliacao('Louis', 8)
-restaurante_praca.receber_avaliacao('Ane', 3)
+bebida_suco = Bebida('Suco de Morango', 5.0, 'grande')
+prato_file = Prato('Filé', 10.0, 'O melhor filé da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_file)
 
 if __name__ == '__main__':
     main()
