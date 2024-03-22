b7b264469b032d8d21ab9044ca5f4bc3a59a974b
Beatriz <beatrizriosmarques@gmail.com>
2024-03-22 18:45:30 -0300

    initial commit

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Morango', 5.0, 'grande')
 prato_file = Prato('Filé', 10.0, 'O melhor filé da cidade')
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_file)
 
 def main():
     print(bebida_suco)
