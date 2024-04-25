205c2060e67f540ebcbfa2d58d552b5559c3bfd8
Beatriz Marques <beatrizriosmarques@gmail.com>
2024-04-25 12:47:12 -0300

    Adicionando Polimorfismo ao codigo

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Morango', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_file = Prato('Filé', 10.0, 'O melhor filé da cidade')
+prato_file.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_file)
 
